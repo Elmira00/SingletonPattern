3ff7ce40f2a6f493b5e4f05f1248ebf62b68c0d5
Ahmed_pz48 <Ahmed_pz48@itstep.edu.az>
2023-04-07 05:35:07 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton instance = new Singleton();
+            Singleton instance1 = Singleton.GetInstance();
+            Singleton instance2 = Singleton.GetInstance();
+            if (instance1.Equals(instance2))
+            {
+                Console.WriteLine("Okay");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    
+    class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+        private static Singleton instance;
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
